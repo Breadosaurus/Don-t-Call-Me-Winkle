805ae1005910cf8ebb05b6c0a020fb34924bf387
rasureiden <amhou@ucsc.edu>
2022-05-11 20:36:19 -0700

    uh trying tilesprite but did a silly hold on!

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,7 +4,17 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
-        this.load.path = './assets/';
+        
+        this.load.path = 'assets/';
+
+        this.load.image('bg_1', 'bg_1.png');
+
+    }
+
+    create() {
+
+        this.scene.start('startScene');
+
     }
 
 }
--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -4,6 +4,14 @@ class Start extends Phaser.Scene {
     }
 
     create() {
+
+        this.clouds = this.add.tileSprite(0, 0, 825, 650, 'bg_1');
+        
+    }
+
+    update() {
+
+        this.clouds.tilePositionX -= scrollSpeed;
         
     }
 }
